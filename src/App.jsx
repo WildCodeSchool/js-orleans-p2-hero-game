e6d14c361db91e27ea2427d74891235d82b5b7e7
ldupas <louisdupas@net-c.com>
2020-10-22 14:17:36 +0200

    project init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className="container">
+      <div className="item">
+        <h1>Hello world !</h1>
+      </div>
+      <div className="item">
+        <p>Welcome to your fresh, lightweight, React App ! &#127752;</p>
+      </div>
+      <div className="item">
+        <p>
+          Start in the <code>App.jsx</code> component !
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
