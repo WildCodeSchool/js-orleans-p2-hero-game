f75effd80a8cc46baf6fdd31c61be1ee4c6e4da7
MartyLucas45 <marty45@hotmail.fr>
2020-10-27 07:28:50 +0100

    27 OCT Navbar OK (Var color et liens à intégrer)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import './App.css';
+import Navbar from './component/navbar/navbar';
 
 function App() {
   return (
     <main className="container">
-      <div className="item">
-        <h1>Hello world !</h1>
-      </div>
-      <div className="item">
-        <p>Welcome to your fresh, lightweight, React App ! &#127752;</p>
-      </div>
-      <div className="item">
-        <p>
-          Start in the <code>App.jsx</code> component !
-        </p>
-      </div>
+      <Navbar />
     </main>
   );
 }
---
+++ src/component/navbar/navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './navbar.css';
+import logo from './Logo_Mini.png';
+
+function Navbar() {
+    return(
+        <div>
+            <nav className="NAVBAR_total" id="Marty">
+                <ul className="NAVBAR_nav">
+                    <div className="NAVBAR_divLogo">
+                        <picture><img className="NAVBAR_logo" src={logo} alt="Logo My Sup'R Heros"/></picture>
+                    </div>
+                    <div className="NAVBAR_rightDiv">
+                        <ul className="NAVBAR_navList">
+                            <li className="NAVBAR_title">JEUX</li>
+                            <li className="NAVBAR_title">PERSONNAGES</li>
+                            <li className="NAVBAR_title">QUIZZ</li>
+                        </ul>
+                    </div>
+                </ul>
+            </nav>
+        </div>
+    )
+    
+};
+
+export default Navbar;
