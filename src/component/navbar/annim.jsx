8bad16782f6b95421af8fcc4c622b320523b477d
MartyLucas45 <marty45@hotmail.fr>
2020-10-28 14:14:22 +0100

    28 Oct fin User Story

--- src/component/navbar/annim.jsx
+++ src/component/navbar/annim.jsx
@@ -7,7 +7,6 @@ function open() {
   const menuBackground = document.querySelector(".NAVBAR_divLogo")
 
   let result = menuBackground.classList.toggle("open");
-  console.log(result)
   
   if(result){
     list.classList.add('open')
@@ -18,84 +17,6 @@ function open() {
     menuBtn.classList.remove('open')
     menuBackground.classList.remove('open')
   }
-
-
-
-
-
-  // if (menuOpen) {
-  //   menuBtn.classList.toggle("open");
-  //   menuOpen= false;
-  //   menuBackground.style.alignItems= 'flex-start';
-  //   menuBackground.animate([
-  //     // keyframes
-  //     { width: '80px' }, 
-  //     { width: '280px' },
-      
-  //   ], { 
-  //     // timing options
-  //     duration: 500,
-  //     iterations: 1,
-  //   });
-  //   logo.animate([
-  //     // keyframes
-  //     { marginTop: '10px' }, 
-  //     { marginTop: '20px' },
-      
-  //   ], { 
-  //     // timing options
-  //     duration: 500,
-  //     iterations: 1,
-  //   });
-  //   logo.style.marginTop='20px';
-  //   menuBackground.animate([
-  //     // keyframes
-  //     {height: '10vh'},
-  //     {height: '100vh'},
-      
-  //   ], { 
-  //     // timing options
-  //     duration: 500,
-  //     iterations: 1,
-  //   });
-  //   menuBackground.style.width='280px'
-  //   menuBackground.style.height='100vh'
-  // } else {
-  //   menuBackground.animate([
-  //     // keyframes
-  //     { width: '280px' }, 
-  //     { width: '80px' },
-      
-  //   ], { 
-  //     // timing options
-  //     duration: 500,
-  //     iterations: 1,
-  //   });
-  //   logo.animate([
-  //     // keyframes
-  //     { marginTop: '10px' }, 
-  //     { marginTop: '20px' },
-      
-  //   ], { 
-  //     // timing options
-  //     duration: 500,
-  //     iterations: 1,
-  //   });
-  //   logo.style.marginTop='20px';
-  //   menuBackground.animate([
-  //     // keyframes
-  //     {height: '100vh'},
-  //     {height: '10vh'},
-      
-  //   ], { 
-  //     // timing options
-  //     duration: 500,
-  //     iterations: 1,
-  //   });
-  //   menuBackground.style.width='80px'
-  //   menuBackground.style.height='10vh'
-  //   menuOpen= true;
-  // }
 };
 
 export default open;
