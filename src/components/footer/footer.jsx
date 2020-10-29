139e895159e10482a6e2db8ff163910d6925f42f
MartyLucas45 <marty45@hotmail.fr>
2020-10-29 08:03:35 +0100

    29 Oct Création Listes / Début

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './footer.css'
+
+function Footer() {
+    return(
+        <div className="FOOTER_total">
+            <ul className="FOOTER_div">
+                <p className="FOOTER_title">MY SUP'R HERO</p>
+                <picture><img src="" alt=""/></picture>
+                <p className="FOOTER_text"> Les Personnages</p>
+                <p className="FOOTER_text">Le Quizz</p>
+            </ul>
+            <ul className="FOOTER_div">
+                <p className="FOOTER_title">MY SUP'R HERO</p>
+                <picture><img src="" alt=""/></picture>
+                <p className="FOOTER_text"> Les Personnages</p>
+                <p className="FOOTER_text">Le Quizz</p>
+            </ul>
+            <ul className="FOOTER_div">
+                <p className="FOOTER_title">ÉQUIPE DE PRODUCTION</p>
+                <p className="FOOTER_text">Sylvie SAIDI</p>
+                <p className="FOOTER_text">Thomas ODE</p>
+                <p className="FOOTER_text">Florent DESMARETS</p>
+                <p className="FOOTER_text">Marty LUCAS</p>
+            </ul>
+        </div>
+    )
+}
+
+export default Footer
