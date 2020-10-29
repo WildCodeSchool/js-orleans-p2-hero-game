1ed5e76b5d59eb5998e2c0e1c2d240627f7f1716
Sylvie-Saidi <sylvie.saidi22@gmail.com>
2020-10-29 19:46:15 +0100

    Quizz complet sprint 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import './App.css';
-
+import Quizz from './components/Quizz';
 function App() {
   return (
     <main className="container">
       <div className="item">
-        <h1>Hello world !</h1>
-      </div>
-      <div className="item">
-        <p>Welcome to your fresh, lightweight, React App ! &#127752;</p>
-      </div>
-      <div className="item">
-        <p>
-          Start in the <code>App.jsx</code> component !
-        </p>
+      <Quizz />
       </div>
     </main>
   );
