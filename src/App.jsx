ac69222b40a96a24d7102a55d17f862d96aed129
MartyLucas45 <marty45@hotmail.fr>
2020-10-29 16:37:52 +0100

    29 Oct Rectification Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css';
 import './main.css'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
-import Jeux from './component/pages/jeux';
-import Accueil from './component/pages/accueil';
-import NavBar from './component/navbar/navbar'
+import Jeux from './components/pages/Jeux';
+import Accueil from './components/pages/Accueil';
+import NavBar from './components/navbar/Navbar'
 
 function App() {
   return (
