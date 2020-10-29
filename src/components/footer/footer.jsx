48d47a0b6e25f88f0cb23502f77de5250e985cab
MartyLucas45 <marty45@hotmail.fr>
2020-10-29 10:37:33 +0100

    29 Oct Avancée Footer OK

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,28 +1,44 @@
 import React from 'react'
 import './footer.css'
+import Github from './github_icon-icons.com_65450.png'
+import WCS from './WCS.png'
 
 function Footer() {
     return(
-        <div className="FOOTER_total">
-            <ul className="FOOTER_div">
-                <p className="FOOTER_title">MY SUP'R HERO</p>
-                <picture><img src="" alt=""/></picture>
-                <p className="FOOTER_text"> Les Personnages</p>
-                <p className="FOOTER_text">Le Quizz</p>
-            </ul>
-            <ul className="FOOTER_div">
-                <p className="FOOTER_title">MY SUP'R HERO</p>
-                <picture><img src="" alt=""/></picture>
-                <p className="FOOTER_text"> Les Personnages</p>
-                <p className="FOOTER_text">Le Quizz</p>
-            </ul>
-            <ul className="FOOTER_div">
-                <p className="FOOTER_title">ÉQUIPE DE PRODUCTION</p>
-                <p className="FOOTER_text">Sylvie SAIDI</p>
-                <p className="FOOTER_text">Thomas ODE</p>
-                <p className="FOOTER_text">Florent DESMARETS</p>
-                <p className="FOOTER_text">Marty LUCAS</p>
-            </ul>
+        <div>
+            <div className="FOOTER_total">
+                <ul className="FOOTER_div">
+                    <p className="FOOTER_title">MY SUP'R HERO</p>
+                    <picture><img src="" alt=""/></picture>
+                    <a href="#list"><p className="FOOTER_text"> Les Personnages</p></a>
+                    <a href="#quizz"><p className="FOOTER_text">Le Quizz</p></a>
+                </ul>
+                <ul className="FOOTER_div">
+                    <p className="FOOTER_title">WILD CODE SCHOOL</p>
+                    <a href="https://www.wildcodeschool.com/fr-FR" target="_blank"><picture><img src={WCS} alt="logo wild code school"/></picture></a>
+                    <p className="FOOTER_text"> Promo Dev 2020 / 2021</p>
+                    <p className="FOOTER_text">Section JavaScript</p>
+                </ul>
+                <ul className="FOOTER_div">
+                    <p className="FOOTER_title">ÉQUIPE DE PRODUCTION</p>
+                    <div className="FOOTER_divDev">
+                        <a href="https://github.com/Sylvie-Saidi" target="_blank"><picture><img className="FOOTER_logoGithub" src={Github} alt="lien github"/></picture></a>
+                        <p className="FOOTER_text">Sylvie SAIDI</p>
+                    </div>
+                    <div className="FOOTER_divDev">
+                        <a href="https://github.com/thomas-ode" target="_blank"><picture><img className="FOOTER_logoGithub" src={Github} alt="lien github"/></picture></a>
+                        <p className="FOOTER_text">Thomas ODE</p>
+                    </div>
+                    <div className="FOOTER_divDev">
+                        <a href="https://github.com/florentdesmarets" target="_blank"><picture><img className="FOOTER_logoGithub" src={Github} alt="lien github"/></picture></a>
+                        <p className="FOOTER_text">Florent DESMARETS</p>
+                    </div>
+                    <div className="FOOTER_divDev">
+                        <a href="https://github.com/MartyLucas45" target="_blank"><picture><img className="FOOTER_logoGithub" src={Github} alt="lien github"/></picture></a>
+                        <p className="FOOTER_text">Marty LUCAS</p>
+                    </div>
+                </ul>
+            </div>
         </div>
     )
 }
